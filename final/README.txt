6c9af244b9afa5ac23eb5ce09a48b89c894f92d9
Vinit Shahdeo <vinitshahdeo@gmail.com>
2018-12-05 05:09:13 +0530

    deleted old files

--- final/README.txt
+++
@@ -1,3 +0,0 @@
-A Pen created at CodePen.io. You can find this one at https://codepen.io/afranceschetti/pen/weXxap.
-
- Funny test with with css keyframe.
--- siri.php
+++
@@ -1,7 +0,0 @@
-<?php
-$response = get("https://montanaflynn-text-to-speech.p.mashape.com/speak?text=Talk+to+me!",
-  array(
-    "X-Mashape-Key" => "LMsQwH77S6msh14woyNKk7uxO7UQp1OytPVjsnBtrIOqGZ1t2i"
-  )
-);
-?>
